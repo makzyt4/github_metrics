8453a42b82a5b169c3252000ccc0132b415ddd53
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-12-07 12:22:26 +0100

    Added index page

---
+++ src/main/java/pl/makzyt/github_metrics/controller/IndexController.java
@@ -0,0 +1,12 @@
+package pl.makzyt.github_metrics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String indexGet() {
+        return "index";
+    }
+}
