ca97b0cd93da54b326193b9b9217c54b53d74c28
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2019-01-03 18:19:21 +0100

    Added working libs

--- src/main/java/pl/makzyt/github_metrics/controller/IndexController.java
+++ src/main/java/pl/makzyt/github_metrics/controller/IndexController.java
@@ -1,11 +1,12 @@
 package pl.makzyt.github_metrics.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String indexGet() {
         return "index";
     }
